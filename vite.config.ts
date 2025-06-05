256a4e366ebf111c91ff9e5412bb67a35830c70d
Trent Breneman <trentbreneman@icloud.com>
2025-06-04 20:52:33 -0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/TrentBreneman/',
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/setupTests.ts',
-    
+  test: { 
+    environment: 'jsdom', 
+    globals: true, 
+    setupFiles: './src/setupTests.ts', 
   },
 });
